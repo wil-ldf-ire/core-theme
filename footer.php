f37dc2c3705cd1905b82efe9e3decfc6f3f7e613
akshay / jigmet <akshay.wildfire@pm.me>
2020-08-23 01:14:54 +0530

    initial commit

---
+++ footer.php
@@ -0,0 +1,10 @@
+
+	<script src="/plugins/jquery.min.js"></script>
+	<script src="/plugins/popper/popper.min.js"></script>
+	<script src="/plugins/moment.js"></script>
+	<script src="/plugins/bootstrap/dist/js/bootstrap.min.js"></script>
+	<script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
+
+	<script src="<?php echo THEME_URL; ?>/js/custom.js?v=<?php echo time(); ?>"></script>
+</body>
+</html>
