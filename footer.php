a730f33088ac9f5ef0ec52cc84783f1e6e6f9452
akshay / jigmet <akshay.wildfire@pm.me>
2020-11-27 01:46:47 +0530

    plugins within theme folder

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
 
-	<script src="/plugins/jquery.min.js"></script>
-	<script src="/plugins/popper/popper.min.js"></script>
-	<script src="/plugins/moment.js"></script>
-	<script src="/plugins/bootstrap/dist/js/bootstrap.min.js"></script>
+	<script src="<?php echo THEME_URL; ?>/plugins/jquery.min.js"></script>
+	<script src="<?php echo THEME_URL; ?>/plugins/popper/popper.min.js"></script>
+	<script src="<?php echo THEME_URL; ?>/plugins/moment.js"></script>
+	<script src="<?php echo THEME_URL; ?>/plugins/bootstrap/dist/js/bootstrap.min.js"></script>
 
 	<script src="<?php echo THEME_URL; ?>/js/custom.js"></script>
 </body>
--- header.php
+++ header.php
@@ -10,8 +10,8 @@
 	<meta property="og:description" content="<?php echo $meta_description; ?>">
 	<meta property="og:image" content="<?php echo $meta_image_url; ?>">
 
-	<link href="/plugins/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-	<link href="/plugins/fontawesome/css/all.min.css" rel="stylesheet">
+	<link href="<?php echo THEME_URL; ?>/plugins/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+	<link href="<?php echo THEME_URL; ?>/plugins/fontawesome/css/all.min.css" rel="stylesheet">
 	<link href="<?php echo THEME_URL; ?>/css/custom.css" rel="stylesheet">
 	<script type="text/javascript">var THEME_URL = "<?php echo THEME_URL; ?>";</script>
 </head>
