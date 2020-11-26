e32c1ddf5d15272a1d65bd80041970e2994c618e
akshay / jigmet <akshay.wildfire@pm.me>
2020-11-27 01:44:53 +0530

    removed flickty

--- footer.php
+++ footer.php
@@ -3,8 +3,7 @@
 	<script src="/plugins/popper/popper.min.js"></script>
 	<script src="/plugins/moment.js"></script>
 	<script src="/plugins/bootstrap/dist/js/bootstrap.min.js"></script>
-	<script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
 
-	<script src="<?php echo THEME_URL; ?>/js/custom.js?v=<?php echo time(); ?>"></script>
+	<script src="<?php echo THEME_URL; ?>/js/custom.js"></script>
 </body>
 </html>
--- header.php
+++ header.php
@@ -12,7 +12,6 @@
 
 	<link href="/plugins/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 	<link href="/plugins/fontawesome/css/all.min.css" rel="stylesheet">
-	<link href="https://unpkg.com/flickity@2/dist/flickity.min.css" rel="stylesheet">
 	<link href="<?php echo THEME_URL; ?>/css/custom.css" rel="stylesheet">
 	<script type="text/javascript">var THEME_URL = "<?php echo THEME_URL; ?>";</script>
 </head>
